2927241c07b83b5917035d42bfea6e57f8bdddc7
NoTalentGeek <mikael.pratama@yahoo.com>
2015-08-25 20:11:56 +0200

    Successfully create object oriented version of the coin inside physics object.

--- js/ObjectCoin.js
+++ js/ObjectCoin.js
@@ -1,7 +1,10 @@
 ObjectCoin                              = function(_x, _y){
 
-    ObjectPhysics.call                  (_x, _y, 'ImageCoin');
+    ObjectPhysics.call                  (this, _x, _y, 'ImageCoin');
+
+    this.body.immovable                 = true;
+    this.body.moves                     = false;
 
 };
-ObjectCoin.prototype                = Object.create(ObjectPhysics.prototype);
-ObjectCoin.prototype.constructor    = ObjectCoin;
+ObjectCoin.prototype                    = Object.create(ObjectPhysics.prototype);
+ObjectCoin.prototype.constructor        = ObjectCoin;
--- js/ObjectPhysics.js
+++ js/ObjectPhysics.js
@@ -1,13 +1,8 @@
 ObjectPhysics                           = function(_x, _y, _sprite){
     
     Phaser.Sprite.call                  (this, game, _x, _y, _sprite);
-
     this.enableBody                     = true;
-
     game.physics.arcade.enable          (this);
-    this.body.immovable                 = true;
-    this.body.moves                     = false;
-    
     this.game.add.existing              (this);
 
 };
